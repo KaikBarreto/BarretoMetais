ac6c7c73bf3e86a1ccff72eaa1a18095f5364d7d
Kaik Barreto <kaikchaides123@gmail.com>
2022-04-21 16:50:30 -0300

    21/04:16:50

--- js/script.js
+++ js/script.js
@@ -90,26 +90,64 @@ $(document).ready(function () {
 
         let scroll = $(window).scrollTop()
 
-        if(scroll > (dataAreaOffset.top - 500) && stop == 0) {
+        if (scroll > (dataAreaOffset.top - 500) && stop == 0) {
 
             circleA.animate(1.0)
             circleB.animate(1.0)
             circleC.animate(1.0)
             circleD.animate(1.0)
 
-        stop = 1
+            stop = 1
         }
 
-  })
+    })
 
     // Parallax
 
-    setTimeout(function() {
+    setTimeout(function () {
 
-        $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
+        $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' })
+        $('#apply-area').parallax({ imageSrc: 'img/pattern.png' })
 
     }, 250)
 
+    // Filtro do portfolio
+    $('.filter-btn').on('click', function() {
+
+        let type = $(this).attr('id')
+        let boxes = $('.project-box')
+
+        $('.main-btn').removeClass('active')
+        $(this).addClass('active')
+
+        if(type == 'dsg-btn') {
+            eachBoxes('dsg', boxes);
+          } else if(type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+          } else if(type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+          } else {
+            eachBoxes('all', boxes);
+          }
+      
+
+          function eachBoxes(type, boxes) {
+
+            if(type == 'all') {
+              $(boxes).fadeIn();
+            } else {
+              $(boxes).each(function() {
+                if(!$(this).hasClass(type)) {
+                  $(this).fadeOut('slow');
+                } else {
+                  $(this).fadeIn();
+                }
+              });
+            }
+          }
+    })
 
 
+    
 })
+
