50992f3667da56da6c87557f18596cc31117e288
Kaik Barreto <kaikchaides123@gmail.com>
2022-05-01 14:58:38 -0300

    changed photo name

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 // Parallax
 
 setTimeout(function () {
-  $('#pattern-img').parallax({ imageSrc: 'img/pattern.png' })
-  $('#company-img').parallax({ imageSrc: './img/empresa.jpg' })
+    $('#pattern-img').parallax({ imageSrc: 'img/pattern.png' })
+    $('#company-img').parallax({ imageSrc: './img/planeta.jpg' })
 }, 250)
 
 // Form de contato
 
 function Enviar() {
 
-  const nome = document.getElementById("nameinput")
-  const phone = document.getElementById("phoneinput")
-  const msg = document.getElementById("msginput")
-  const confirm = document.getElementById("confirmation-text")
+    const nome = document.getElementById("nameinput")
+    const phone = document.getElementById("phoneinput")
+    const msg = document.getElementById("msginput")
+    const confirm = document.getElementById("confirmation-text")
 
-  if (nome.value != "" && email.value != "" && msg != "") {
-    confirm.style.display = "inline-block"
+    if (nome.value != "" && email.value != "" && msg != "") {
+        confirm.style.display = "inline-block"
 
-    setTimeout(function () {
-      confirm.style.display = "none"
-    }, 4000)
+        setTimeout(function () {
+            confirm.style.display = "none"
+        }, 4000)
 
-    nome.value = ""
-    phone.value = ""
-    msg.value = ""
-  }
-  document.getElementById("msg-box").focus()
+        nome.value = ""
+        phone.value = ""
+        msg.value = ""
+    }
+    document.getElementById("msg-box").focus()
 
 }
